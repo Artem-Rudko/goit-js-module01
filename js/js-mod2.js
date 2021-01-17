a27ee9c01b4a4ed96fab760757ebe29d762b1aa3
Artem-Rudko <armoks@ukr.net>
2021-01-17 21:44:13 +0200

    Решает задачи модуля2

---
+++ js/js-mod2.js
@@ -0,0 +1,406 @@
+let starter=0;
+let module=2;
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function printTaskNumber(0) {
+//     return console.log(`Module:${module}; Task:${starter+=1}`); 
+// }
+// printTaskNumber();
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function splitMessage(message, delimeter) {
+//     let words;
+//     // Пиши код ниже этой строки
+//     words = message.split(delimeter);
+    
+//     // Пиши код выше этой строки
+//     return console.log(words);
+// } 
+// splitMessage('Манго спешит на поезд', ' ');
+// splitMessage('Манго', '');
+// splitMessage('лучшее_за_неделю', '_');
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//     // Пиши код ниже этой строки
+// const messageInWords = message.split(" ");
+// const totalPrice = messageInWords.length * pricePerWord;
+// return console.log(totalPrice);
+  
+//     // Пиши код выше этой строки
+//   }
+
+//   calculateEngravingPrice('JavaScript у меня в крови', 10);
+//   calculateEngravingPrice('JavaScript у меня в крови', 20);
+//   calculateEngravingPrice('Веб-разработка это творческая работа', 40);
+//   calculateEngravingPrice('Веб-разработка это творческая работа', 20);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function makeStringFromArray(array, delimeter) {
+//     let string;
+//     // Пиши код ниже этой строки
+//    string = array.join(delimeter);
+
+//     // Пиши код выше этой строки
+//     return console.log(string);
+//   }
+//   makeStringFromArray(['Манго', 'спешит', 'на', 'поезд'], ' ');
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function slugify(title) {
+    // Пиши код ниже этой строки
+
+//    const titleArray = title.split(' ');
+//    const slug = titleArray.join('-');
+//    return console.log(slug.toLowerCase()); 
+
+// const slug = title.toLowerCase().split(' ').join('-');
+//   return slug;
+    // Пиши код выше этой строки
+//   }
+//   slugify('Массивы для новичков');
+//   slugify('Английский для разработчика');
+//   slugify('Десять секретов JavaScript');
+//   slugify('Как стать JUNIOR разработчиком за ДВЕ НЕДЕЛИ');
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин', 'банан'];
+// // Пиши код ниже этой строки
+// const firstTwoEls = fruits.slice(0,2);
+// const nonExtremeEls = fruits.slice(1,fruits.length-1);
+// const lastThreeEls = fruits.slice(-3);
+// console.log(firstTwoEls);
+// console.log(nonExtremeEls);
+// console.log(lastThreeEls);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// const oldClients = ['Манго', 'Аякс', 'Поли', 'Киви'];
+// const newClients = ['Персик', 'Хьюстон'];
+
+// const allClients = oldClients.concat(newClients);
+// console.log(allClients); // Дополни эту строку
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Пиши код ниже этой строки
+// let newArray=firstArray.concat(secondArray)
+// return console.log(newArray.slice(0,maxLength));
+//     // Пиши код выше этой строки
+//   }
+//   makeArray(['Манго', 'Поли'], ['Аякс', 'Челси'], 3);
+//   makeArray(['Манго', 'Поли', 'Хьюстон'], ['Аякс', 'Челси'], 4);
+//   makeArray(['Манго'], ['Аякс', 'Челси', 'Поли', 'Хьюстон'], 3);
+//   makeArray(['Земля', 'Юпитер'], ['Нептун', 'Уран'], 2);
+//   makeArray(['Земля', 'Юпитер'], ['Нептун', 'Уран'], 4);
+//   makeArray(['Земля', 'Юпитер'], ['Нептун', 'Уран', 'Венера'], 0);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) { // Дополни эту строку
+//   console.log(i);
+// }
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function calculateTotal(number) {
+//     // Пиши код ниже этой строки
+//   let sum=0;
+//     for (let i = 0; i <= number; i += 1) {
+//      sum += i; 
+//     }
+//     console.log(sum);
+//     return sum;
+
+//       // Пиши код выше этой строки
+//   }
+//   calculateTotal(3);
+//   calculateTotal(7);
+//   calculateTotal(18);
+//   calculateTotal(24);
+//   calculateTotal();
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+
+// for (let i = 0; i < fruits.length; i +=1) { // Дополни эту строку
+//   const fruit = fruits[i]; // Дополни эту строку
+//   console.log(fruit);
+// }
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function calculateTotalPrice(order) {
+//     let total = 0;
+//     // Пиши код ниже этой строки
+//   for (let i = 0; i < order.length; i += 1) {
+//       total += order[i];
+//   }
+// console.log(total);
+//     // Пиши код выше этой строки
+//     return total;
+//   }
+//   calculateTotalPrice([12, 85, 37, 4]);
+//   calculateTotalPrice([164, 48, 291]);
+//   calculateTotalPrice([412, 371, 94, 63, 176]);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function findLongestWord(string) {
+//     // Пиши код ниже этой строки
+//     const array = string.split(' ');
+//     let indexOfTheLongestWord = 0;
+//     for (let i=1; i <= array.length-1; i += 1) {
+//         if (array[i].length > array[indexOfTheLongestWord].length) {
+//             indexOfTheLongestWord = i;
+//         }
+//     }
+    
+//   return console.log(array[indexOfTheLongestWord]);
+//     // Пиши код выше этой строки
+//   }
+
+//   findLongestWord('The quick brown fox jumped over the lazy dog');
+//   findLongestWord('Google do a roll');
+//   findLongestWord('May the force be with you');
+  
+//* assistance
+//   const string = 'javascript';
+
+//   for (const character of string) {
+//     console.log(character);
+//   }
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function createArrayOfNumbers(min, max) {
+//     const numbers = [];
+//     // Пиши код ниже этой строки
+//   for (let i = min; i <= max; i += 1){
+//     numbers.push(i);
+    
+//   }
+//   console.log(numbers);
+//   // Пиши код выше этой строки
+//     return numbers;
+//   }
+//   createArrayOfNumbers(14, 17);
+//   createArrayOfNumbers(29, 34);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function filterArray(numbers, value) {
+//   // Пиши код ниже этой строки
+// const filteredArray = [];
+// for (let i = 0; i <= numbers.length; i += 1) {
+//   if (numbers[i] > value) {
+//     filteredArray.push(numbers[i]);
+//   }
+ 
+// }
+// console.log(filteredArray);
+// return filteredArray;
+//   // Пиши код выше этой строки
+// }
+// filterArray([1, 2, 3, 4, 5], 3);
+// filterArray([1, 2, 3, 4, 5], 4);
+// filterArray([1, 2, 3, 4, 5], 5);
+// filterArray([12, 24, 8, 41, 76], 38);
+// filterArray([12, 24, 8, 41, 76], 20);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function checkFruit(fruit) {
+//   const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+
+//   return console.log(fruits.includes(fruit)); // Дополни эту строку
+// }
+// checkFruit('мандарин');
+// checkFruit('слива');
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function getCommonElements(array1, array2) {
+//   // Пиши код ниже этой строки
+// let commonArray = [];
+// for (let k = 0; k < array1.length; k += 1) {
+//   if (array2.includes(array1[k])) {
+//     commonArray.push(array1[k]);
+//   }
+// }
+//   console.log(commonArray);
+//   return commonArray;
+//   // Пиши код выше этой строки
+// }
+// getCommonElements([1, 2, 3], [2, 4]);
+// getCommonElements([1, 2, 3], [2, 1, 17, 19]);
+// getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]);
+// getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]);
+// getCommonElements([1, 2, 3], [10, 20, 30]);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Пиши код ниже этой строки
+
+//   for (const item of order) {
+//     total += item;
+//   }
+
+//   // Пиши код выше этой строки
+//   return console.log(total);
+// }
+
+//   calculateTotalPrice([12, 85, 37, 4]);
+//   calculateTotalPrice([164, 48, 291]);
+//   calculateTotalPrice([412, 371, 94, 63, 176]);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function filterArray(numbers, value) {
+//   // Пиши код ниже этой строки
+//   const filteredNumbers = [];
+
+//   for (const item of numbers) {
+//     const number = item;
+
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+
+//   return console.log(filteredNumbers);
+//   // Пиши код выше этой строки
+// }
+// filterArray([1, 2, 3, 4, 5], 3);
+// filterArray([1, 2, 3, 4, 5], 4);
+// filterArray([1, 2, 3, 4, 5], 5);
+// filterArray([12, 24, 8, 41, 76], 38);
+// filterArray([12, 24, 8, 41, 76], 20);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 4;
+// const d = 12 % 7;
+// const e = 8 % 3;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function getEvenNumbers(start, end) {
+//   // Пиши код ниже этой строки
+//   let array = [];
+// for (let i = start; i <= end; i += 1){
+//   if (i % 2 === 0) {
+//     array.push(i); 
+//   }
+// }
+//  return console.log(array);
+//   // Пиши код выше этой строки
+// }
+// getEvenNumbers(2, 5);
+// getEvenNumbers(3, 11);
+// getEvenNumbers(6, 12);
+// getEvenNumbers(8, 8);
+// getEvenNumbers(7, 7);
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// const start = 6;
+// const end = 27;
+// let number;
+
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break;
+//   }
+// }
+
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function findNumber(start, end, divisor) {
+//   // Пиши код ниже этой строки
+
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       console.log(i);
+//       return i;
+//     }
+//   }
+//   // Пиши код выше этой строки
+// }
+
+// findNumber(2, 6, 5);
+// findNumber(8, 17, 3);
+// findNumber(6, 9, 4);
+// findNumber(16, 35, 7);
+
+console.log(`Module:${module}; Task:${starter+=1}`);
+
+// function includes(array, value) {
+//   // Пиши код ниже этой строки
+
+// for (let i = 0; i <= array.length; i += 1) {
+//   if (array[i] === value) {
+//     return console.log(true);
+//   }
+// }
+// return console.log(false);    
+// }
+//   // Пиши код выше этой строки
+//   includes([1, 2, 3, 4, 5], 3);
+//   includes([1, 2, 3, 4, 5], 17);
+//   includes(['Земля', 'Марс', 'Венера', 'Юпитер', 'Сатурн'], 'Юпитер');
+//   includes(['Земля', 'Марс', 'Венера', 'Юпитер', 'Сатурн'], 'Уран');
+//   includes(['яблоко', 'слива', 'груша', 'апельсин'], 'слива');
+//   includes(['яблоко', 'слива', 'груша', 'апельсин'], 'киви');
